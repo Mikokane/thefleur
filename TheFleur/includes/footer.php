386f27a6753bb6240bf2f11ebed6182f0a9e95bd
Mikokane <80952994+Mikokane@users.noreply.github.com>
2022-05-31 22:18:34 +0100

    Update footer.php

--- TheFleur/includes/footer.php
+++ TheFleur/includes/footer.php
@@ -1,4 +1,4 @@
 <footer id="footer" class="footer">
         <span class="copyrights">&copy; 2020 - LaFleur</span>
-        <a href="#" class="conditions">Condition générales de vente</a>
+        <a href="admin_area/CGV/conditions_générales_de_vente.pdf" download="conditions_générales_de_vente.pdf" class="conditions">Condition générales de vente</a>
     </footer>
